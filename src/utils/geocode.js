88ee43e81551dbb1397bbf102d951bd174cf1e6e
Novica Ovuka <novica.ovuka.4@gmail.com>
2019-08-17 13:32:15 -0700

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoibm92aWNhb3Z1a2EiLCJhIjoiY2p6N2NiYmI2MDU4djNidDZjMXc4MXcybyJ9.uM-c8hJ7xNH_IWDIYuLoJQ&limit=1'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', '')
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', '')
+        } else {
+            callback('', {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
